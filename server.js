14941725e61fb4b1fb5a77b164a7477395eb8d5d
Yue <771397803@qq.com>
2020-05-12 23:45:01 -0400

    connecting to db

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const postgres = require("postgres")
 const app = express();
 const knex = require('knex')
 
-
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = 0; 
 const db = knex({
   client: 'pg',
   connection: {
